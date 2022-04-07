a40d78db9829d23786a56bcf9619156e97eaeee1
Franco carrasco <franco.carrasco.obreque@alumnos.sip.cl>
2022-04-07 11:52:29 -0400

    cambio 1

---
+++ Franco_Carrasco_VS_JsPrediction.js
@@ -0,0 +1,21 @@
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}copy
+//Prediccion 1.- Naci en 1980//
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+}
+//Prediccion 2.- Naci en 1980//
+
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+//Prediccion 3.- ¡Sumando numeros!
+//num1 is 10
+//num2 is 20
+//30//
