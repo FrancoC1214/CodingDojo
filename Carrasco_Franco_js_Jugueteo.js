e8c2ba5dd291e96e247ccd251f356f4e835f1491
Franco carrasco <franco.carrasco.obreque@alumnos.sip.cl>
2022-04-14 11:19:55 -0400

    desafio uwu

---
+++ Carrasco_Franco_js_Jugueteo.js
@@ -0,0 +1,9 @@
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(Metros) {
+    if (alturaNiño = 52) {
+        console.log("¡Subete, chico!")
+    }
+    else if (alturaNiño > 52) {
+        console.log(NO)
+    }
+}
+muestraSiElNiñoPuedeSubirALaMontañaRusa(777)
